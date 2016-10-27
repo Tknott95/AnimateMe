0926223172c67569aee30b2ff94ac56bdcb79e73
tknott95 <tknott95@hotmail.com>
2016-10-27 15:18:24 -0600

    Hard Coded Trigger Working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('signal', [
+      state('go', style({
+        'background-color':'green'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app works!';
