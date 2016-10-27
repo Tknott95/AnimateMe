f03c8e1ec7b870068b4a08d506c7dc53defc45c2
tknott95 <tknott95@hotmail.com>
2016-10-27 15:49:11 -0600

    Keyframe Practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,42 @@
-import { Component, trigger, state, style, transition, animate } from '@angular/core';
+import { Component, keyframes, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   animations: [
-    trigger('signal', [
+    trigger('signal', [    // Take out void tranisiton if you dont want this
+       state('void', style({
+        'transform': 'translateY(-100%)'
+      })),
       state('go', style({
-        'background-color': 'green'
+        'background-color': 'green',
+        'width': '20%'
       })),
       state('stop', style({
-        'background-color': 'red'
+        'background-color': 'red',
+        'width': '25%'
       })),
-      transition('* => *', animate(500))
+      transition('void <=> *', animate(4000, keyframes([
+        style({'transform': 'scale(.4)'}), //medium
+        style({'transform': 'scale(.9)'}), //fasrt
+        style({'transform': 'scale(.5)'}) //slow
+      ]))),
+      transition('* => *', animate('0s 1s ease-in')) //ease-in is and can be a cubic-bezier(0,0,0,) 
     ])
   ]
 })
 export class AppComponent {
-  signal = 'stop';
-
+  signal: string;
+  isHere = false;
   onGoClick(){
+    
     this.signal = 'go';
   }
   onStopClick(){
     this.signal = 'stop';
+  }
+  onToggleClick(){
+    this.isHere = !this.isHere;
   }
 }
