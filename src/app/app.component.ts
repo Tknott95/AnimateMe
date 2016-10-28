f00e4d9fe4ba232b5de29f7d70fe534092ddf37c
tknott95 <tknott95@hotmail.com>
2016-10-27 21:26:48 -0600

    Multple Animations complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Component, keyframes, trigger, state, style, transition, animate } from
   animations: [
     trigger('signal', [    // Take out void tranisiton if you dont want this
        state('void', style({
-        'transform': 'translateY(-100%)'
+        'transform': 'translateY(-100%)',
+        'background-color': 'black'
       })),
       state('go', style({
         'background-color': 'green',
@@ -17,13 +18,13 @@ import { Component, keyframes, trigger, state, style, transition, animate } from
         'background-color': 'red',
         'width': '25%'
       })),
-      transition('void <=> *', animate(4000, keyframes([
-        style({'transform': 'scale(.4)'}), //medium
-        style({'transform': 'scale(.9)'}), //fasrt
-        style({'transform': 'scale(.5)'}) //slow
+      transition('void <=> *', animate(1000, keyframes([
+        style({'transform': 'scale(.4)'}), // medium
+        style({'transform': 'scale(.9)'}), // fast
+        style({'transform': 'scale(.5)'}) // slow
       ]))),
-      transition('* => *', animate('0s 1s ease-in')) //ease-in is and can be a cubic-bezier(0,0,0,) 
-    ])
+      transition('go <=> stop', animate('.5s 0s ease-in')) // ease-in is and can be a cubic-bezier(0,0,0,) 
+    ]) // how long  delay   cubic-beziar/type
   ]
 })
 export class AppComponent {
